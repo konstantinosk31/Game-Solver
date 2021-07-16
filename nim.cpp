97eab75ddd2d994c3734f73eb59274ad082542ab
Konstantinos Kritharidis <konstantinosk31@gmail.com>
2021-07-16 19:46:29 +0300

    Finished classes and sub-classes and tuned VARMAX

--- nim.cpp
+++ nim.cpp
@@ -14,6 +14,8 @@ typedef pair<int, int> ii;
 
 #define state vector<int>
 
+const int MAXNUM = 500;
+
 bool disjoint = false;
 
 int mex(vector<int> s); //minimum excluded value in a vector of non-negative integers
@@ -53,13 +55,24 @@ class game{
         }
 
     public:
-        int VARMAX = 50; //due to memory and time restrictions, the max size of variables for possible moves is restricted
+        int VARMAX; //due to memory and time restrictions, the max size of variables for possible moves is restricted
+        int size;
+
+        void setVARMAX(){
+            VARMAX = (int)pow(int(2*MAXNUM/size), (double)2/size);
+        }
+
+        game(){}
+
+        game(int size) : size(size){
+            setVARMAX();
+        }
 
         void printX(){ //prints all possible game states
             for(auto x : X) printx(x);
         }
 
-        void initX(state x0){ //initializes the game states vector
+        virtual void initX(state x0){ //initializes the game states vector
             X.pb(x0);
             generateX(x0);
             sort(rall(X));
@@ -70,12 +83,12 @@ class game{
             for(auto x : f) printx(x);
         }
         
-        int initg(state x0){ //initializes Sprague-Grundy array
+        virtual int initg(state x0){ //initializes Sprague-Grundy array
             SG.resize(X.size(), -1);
             return g(x0);
         }
 
-        int g(state x){ //calculates sprague-grundy value of a state
+        virtual int g(state x){ //calculates sprague-grundy value of a state
             if(SG[id(x)] != -1) return SG[id(x)];
             if(F(x).empty()){
                 SG[id(x)] = 0;
@@ -87,7 +100,7 @@ class game{
             return SG[id(x)];
         }
 
-        vector< state > F(state x0){ //creates a vector with all states that we can go to in 1 move from our current state
+        virtual vector< state > F(state x0){ //creates a vector with all states that we can go to in 1 move from our current state
             vector< state > f;
             for(auto move : moves){
                 state x = x0;
@@ -97,16 +110,85 @@ class game{
             return f;
         }
 
-        void addGameMove(state temp){
+        virtual void addMove(state temp){
             moves.pb(temp);
         }
 
-        void addGameTerm(state temp){
+        virtual void addTerm(state temp){
             term.pb(temp);
         }
 };
 
-vector<game> Games; //Games[0] is the main game. If the games are disjoint, Games[1...size] are the individual games
+class sumGame : public game{
+    public:
+        vector<game> games;
+        int size;
+
+        void setVARMAX(){
+            int val = MAXNUM;
+            VARMAX = val;
+            for(auto game : games) game.VARMAX = val;
+        }
+
+        sumGame(int size) : size(size){
+            games.resize(size);
+            setVARMAX();
+        }
+
+        sumGame(vector<game> games) : games(games){
+            size = games.size();
+            setVARMAX();
+        }
+
+        void initX(state x0){
+            for(int i = 0; i < size; i++){
+                this->games[i].initX({x0[i]});
+            }
+        }
+
+        vector< state > F(state x0){
+            int size = x0.size();
+            vector< state > f;
+            for(int i = 0; i < size; i++){
+                for(auto next : games[i].F({x0[i]})){
+                    state x = x0;
+                    x[i] = next[0];
+                    f.pb(x);
+                }
+            }
+            return f;
+        }
+
+        int g(state x0){
+            int nimsum = 0, size = x0.size();
+            for(int i = 0; i < size; i++){
+                nimsum ^= games[i].g({x0[i]});
+            }
+            return nimsum;
+        }
+
+        int initg(state x0){
+            int nimsum = 0;
+            for(int i = 0; i < size; i++){
+                nimsum ^= this->games[i].initg({x0[i]});
+            }
+            return nimsum;
+        }
+
+        void addMove(state temp){
+            for(int i = 0; i < temp.size(); i++){
+                if(temp[i] != 0) this->games[i].addMove({temp[i]});
+            }
+        }
+
+        void addTerm(state temp){
+            for(int i = 0; i < temp.size(); i++){
+                this->games[i].addTerm({temp[i]});
+            }
+        }
+};
+
+game* Game;
 
 int mex(vector<int> s){ //minimum excluded value in a vector of non-negative integers
     int n = 0;
@@ -118,30 +200,15 @@ int mex(vector<int> s){ //minimum excluded value in a vector of non-negative int
 }
 
 void printx(state x){ //prints a game state
-            for(auto el : x) printf("%d ", el);
-            printf("\n");
-        }
+    for(auto el : x) printf("%d ", el);
+    printf("\n");
+}
 
 void getDisjoint(int size){
     printf("Enter \"1\" if there are no moves that affect more than 1 heap in a state, \"0\" otherwise: ");
     scanf("%d", &disjoint);
-    if(!disjoint){
-        Games.resize(1);
-        Games[0].VARMAX = 50;
-    }
-    else{
-        Games.resize(size+1);
-        for(int i = 0; i < size+1; i++) Games[i].VARMAX = 1000;
-    }
-}
-
-void addMove(state temp){
-    if(!disjoint) Games[0].addGameMove(temp);
-    else{
-        for(int i = 0; i < temp.size(); i++){
-            if(temp[i] != 0) Games[i+1].addGameMove({temp[i]});
-        }   
-    }
+    if(!disjoint) Game = new game(size);
+    else Game = new sumGame(size);
 }
 
 void getMoves(int size){ //the user inputs the available moves in the game
@@ -156,18 +223,18 @@ void getMoves(int size){ //the user inputs the available moves in the game
         for(int i = 0; i < fsize; i++){
             state temp(size);
             for(int j = 0; j < size; j++) scanf("%d", &temp[j]);
-            addMove(temp);
+            Game->addMove(temp);
         }
     }
     else if(varMoves == 1){
-        printf("Name your variable v. Note that max variable value is %d.\n", Games[0].VARMAX);
+        printf("Name your variable v. Note that max variable value is %d.\n", Game->VARMAX);
         printf("Enter valid moves (to stop enter \"stop\"):\n");
         while(true){
             vector<string> tempv(size);
             cin>>tempv[0];
             if(tempv[0][0] == 's') break;
             for(int j = 1; j < size; j++) cin>>tempv[j];
-            for(int v = 1; v <= Games[0].VARMAX; v++){
+            for(int v = 1; v <= Game->VARMAX; v++){
                 state temp(size);
                 for(int j = 0; j < size; j++){
                     if((tempv[j][0] == 'v') || (tempv[j][0] == '-' && tempv[j][1] == 'v')){
@@ -177,18 +244,9 @@ void getMoves(int size){ //the user inputs the available moves in the game
                         else temp[j] = -v;
                     } else temp[j] = stoi(tempv[j]);
                 }
-                addMove(temp);
+                Game->addMove(temp);
             }
         }
-    }
-}
-
-void addTerm(state temp){
-    if(!disjoint) Games[0].addGameTerm(temp);
-    else{
-        for(int i = 0; i < temp.size(); i++){
-            if(temp[i] != 0) Games[i+1].addGameTerm({temp[i]});
-        }   
     }
 }
 
@@ -200,89 +258,38 @@ void getTerminalStates(int size){ //the user inputs the terminal states of the g
     for(int i = 0; i < nterm; i++){
         state temp(size);
         for(int j = 0; j < size; j++) scanf("%d", &temp[j]);
-        addTerm(temp);
+        Game->addTerm(temp);
     }
 }
 
 void solve(state x0){
     int nimsum = 0, size = x0.size();
-    if(!disjoint){
-        Games[0].initX(x0);
-        nimsum = Games[0].initg(x0);
-    }
-    else{
-        for(int i = 1; i <= size; i++){
-            Games[i].initX({x0[i-1]});
-            nimsum ^= Games[i].initg({x0[i-1]});
-        }
-    }
+    Game->initX(x0);
+    nimsum = Game->initg(x0);
     printf("SG value of initial state is %d\n", nimsum);
     if(nimsum != 0) printf("Player 1 wins if they play optimally!\n");
     else printf("Player 2 wins if they play optimally!\n");
 }
 
-vector< state > sumF(state x0){
-    int size = x0.size();
-    vector< state > f;
-    for(int i = 1; i < size+1; i++){
-        for(auto next : Games[i].F({x0[i-1]})){
-            state x = x0;
-            x[i-1] = next[0];
-            f.pb(x);
-        }
-    }
-    return f;
-}
-
-int sumg(state x0){
-    int nimsum = 0, size = x0.size();
-    for(int i = 1; i <= size; i++){
-        nimsum ^= Games[i].g({x0[i-1]});
-    }
-    return nimsum;
-}
-
 void strategy(state x){ //prints the optimal strategy and how the game would be
     int size = x.size();
-    if(!disjoint){
-        printf("SG = %d | ", Games[0].g(x));
-        printx(x);
-        if(Games[0].F(x).empty()) return;
-        if(Games[0].g(x) != 0){ //Current player wins if he plays optimally!
-            for(state y : Games[0].F(x)){
-                if(Games[0].g(y) == 0){
-                    strategy(y);
-                    return;
-                }
+    printf("SG = %d | ", Game->g(x));
+    printx(x);
+    if(Game->F(x).empty()) return;
+    if(Game->g(x) != 0){ //Current player wins if he plays optimally!
+        for(state y : Game->F(x)){
+            if(Game->g(y) == 0){
+                strategy(y);
+                return;
             }
-            assert(false); //WE SHOULD NEVER GET HERE
         }
-        else{ //Previous player wins if he plays optimally, so just choose something at random
-            int i = rand()%Games[0].F(x).size();
-            state y = Games[0].F(x)[i];
-            strategy(y);
-            return;
-        }
+        assert(false); //WE SHOULD NEVER GET HERE
     }
-    else{
-        printf("SG = %d | ", sumg(x));
-        printx(x);
-        if(sumF(x).empty()) return;
-        if(sumg(x) != 0){ //Current player wins if he plays optimally!
-            for(state y : sumF(x)){
-                if(sumg(y) == 0){
-                    strategy(y);
-                    return;
-                }
-            }
-            assert(false); //WE SHOULD NEVER GET HERE
-        }
-        else{ //Previous player wins if he plays optimally, so just choose something at random
-            int i = rand()%sumF(x).size();
-            state y = sumF(x)[i];
-            strategy(y);
-            return;
-        }
+    else{ //Previous player wins if he plays optimally, so just choose something at random
+        int i = rand()%Game->F(x).size();
+        state y = Game->F(x)[i];
+        strategy(y);
+        return;
     }
 }
 
